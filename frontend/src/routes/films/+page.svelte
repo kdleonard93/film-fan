aee131293897d0f8459b4d01f39a608da89e420d
Kyle <kdleonard@icloud.com>
2024-01-04 01:49:27 -0600

    Frontend Setup

---
+++ frontend/src/routes/films/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+
+  let film = "American Gangsta";
+</script>
+
+<h1 class="text-secondary-600 font-semibold text-3xl sm:text-4xl md:text-5xl">
+  {film}
+</h1>
